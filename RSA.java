37a1ce17112230bf45815248db4f05434f9c8eeb
yc357 <yc357@nau.edu>
2017-02-14 12:28:09 -0700

    Create RSA.java

---
+++ RSA.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class RSA{
+  public static void main(String() args){
+  
+ public int enKey;
+ private int deKey;
+ int p = 61;
+ int q = 53;
+ int n;
+ int dd;
+ 
+ n = 61*53;
+ 
+ dd = lcm
+ 
+ 
+ 
+  
+  
